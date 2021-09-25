c46fde46c082f52dd7c489dcd5e7d224133f0cb5
jklwer <ijij1135@naver.com>
2021-09-25 18:26:48 +0900

    [이지원]dev commit

--- WebContent/WEB-INF/views/todo/todo-modify.jsp
+++ WebContent/WEB-INF/views/todo/todo-modify.jsp
@@ -6,9 +6,9 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 
-<!-- 
+
 <link rel="stylesheet" href="/resources/css/todo/reset.css"> 
--->
+
 
 <link rel="stylesheet" href="/resources/css/todo/test.css">
 
@@ -18,22 +18,26 @@
  <script src='https://cdn.jsdelivr.net/combine/npm/fullcalendar@5.9.0/main.min.js,
 											npm/fullcalendar@5.9.0/locales-all.min.js'></script>
 
-</head>
+</head> 
 <body>
 
 
-<input type="text" placeholder="일정을 입력하세요">
-<p><input type="text" placeholder="카테고리"></p>
+<p><input type="text" placeholder="일정을 입력하세요"></p>
+
+<p><input type="date" id="dateInput"></p>
+<p><input type="time" id="timeInput"></p>
 
 <button id="addBtn">일정  추가</button>
+<p><button id="sortBtn">일정 정렬</button></p>
 
 <ul></ul>
 <table id="todoTable">
 	<tr>
-		<td class="td">checkbox</td>
-		<td class="td">to-do</td>
-		<td class="td">category</td>
-		<td class="td">delete</td>
+		<td></td>
+		<td>Date</td>
+		<td>Time</td>
+		<td>to-do</td>
+		<td>delete</td>
 	</tr>
 </table>
 
--- WebContent/resources/js/todo/calendar.js
+++ WebContent/resources/js/todo/calendar.js
@@ -16,10 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {	//document.ready
     	plugins: ['interaction', 'dayGrid', 'interactionPlugin'];
       
         var calendarEl = document.getElementById('calendar');
-        var calendarEl = document.getElementById('calendar');
-    	
-
-
 
         var calendar = new FullCalendar.Calendar(calendarEl, {
           headerToolbar: {
@@ -29,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {	//document.ready
 
           },
           
-          initialDate: '2020-09-12',
+          //initialDate: '2020-09-12',
 
           initialView: 'dayGridMonth',
           locale: 'ko',	//한국
@@ -135,62 +131,222 @@ document.addEventListener('DOMContentLoaded', function() {	//document.ready
 todoMain();
 
 function todoMain(){
-	let inputElem, inputElem2, button, ulElem;
+	let inputElem, dateInput, timeInput, addButton, sortButton, ulElem, todoList = [];
+	
 	
 	getElements();
 	addListeners();
-	
+	load();
+	renderRows();
 	
 	function getElements(){
 		inputElem = document.getElementsByTagName("input")[0];
-		inputElem2 = document.getElementsByTagName("input")[1];
-		button = document.getElementById("addBtn");
-		ulElem = document.getElementsByTagName("ul")[0];
-		
+		dateInput = document.getElementById("dateInput");
+		timeInput = document.getElementById("timeInput");
+		addButton = document.getElementById("addBtn");
+		sortButton = document.getElementById("sortBtn");
+//		ulElem = document.getElementsByTagName("ul")[0];
 	}
 	
 	
 	function addListeners(){
-		button.addEventListener("click", addEntry)
+		addButton.addEventListener("click", addEntry);
+		sortButton.addEventListener("click", sortEntry);
 	}
-	
-	
 	
 	function addEntry(event){
 		
-		let flag = true;
+		//let flag = true;
 		
 		let inputValue = inputElem.value;
 		inputElem.value = "";
+		
+		let dateValue = dateInput.value;
+		dateInput.value = "";
+		
+		let timeValue = timeInput.value;
+		timeInput.value = "";
 
-		let inputValue2 = inputElem2.value;
-		inputElem2.value = "";
+		let obj = {
+			id : _uuid(),	//체크박스의 데이터 아이디 
+			todo : inputValue,
+			date : dateValue,
+			time : timeValue,
+			done : false	
+		}
+		renderRow(obj);
+
+		todoList.push(obj);
+		
+		save();
 	
+
+	}		
+	
+		function save(){
+			let stringified = JSON.stringify(todoList);	//todoList는 String타입이므로 배열의 메서드인 forEach 사용불가므로 
+														//todoList 값들을 String으로 만들어 배열화함 
+			localStorage.setItem("todoList", stringified);
+			
+		}
+		
+		function load(){
+			//value값은 아직 String이므로
+			let retrieved = localStorage.getItem("todoList");
+			todoList = JSON.parse(retrieved)
+			console.log(typeof todoList);	//오브젝트 
+			if(todoList == null){
+				todoList = [];
+			}
+			
+		}
+		
+		function renderRows(){
+			todoList.forEach(todoObj =>{
+			
+			//	let todoEntry = todoObj["todo"];
+				renderRow(todoObj);
+			})
+			
+		}
+		
+		function renderRow({todo : inputValue, id, date, time, done} = obj){
+/*			
+		let inputValue = obj.todo;
+
+		let {todo : inputValue} = obj;
+*/		
+		
 		//add a new row
 		let table = document.getElementById("todoTable");
 		let trElem = document.createElement("tr");
-		table.appendChile(trElem);
-		
-		
+		table.appendChild(trElem);
 		
 		//checkbox cell
 		let checkboxElem = document.createElement("input");
 		checkboxElem.type = "checkbox";
+		checkboxElem.addEventListener("click", checkboxClickCallback);	//체크박스 누르면 글씨에 줄침
+		checkboxElem.dataset.id = id;
 		let tdElem1 = document.createElement("td");
-		tdElem1.appendchild(checkboxElem);
-		trElem.appendchild(tdElem1);
-		
+		tdElem1.appendChild(checkboxElem);
+		trElem.appendChild(tdElem1);
+
 		//to-do cell
 		let tdElem2 = document.createElement("td");
 		tdElem2.innerText = inputValue;
-		trElem.appendchild(tdElem2);
+		trElem.appendChild(tdElem2);
 		
-		//category cell
-		let tdElem3 = document.createElement("td");
-		tdElem3.innerText = inputValue2;
-		trElem.appendchild(tdElem3);
+		
+		//date cell
+		let dateElem = document.createElement("td");
+		dateElem.innerText = date;
+		trElem.appendChild(dateElem);
+		
+		
+		//time cell
+		let timeElem = document.createElement("td");
+		timeElem.innerText = time;
+		trElem.appendChild(timeElem);
+		
+		
 		//delete cell
+		let spanElem = document.createElement("span");	//삭제 span태그로 감쌈
+		spanElem.innerText = " 삭제";
+		spanElem.addEventListener("click", deleteItem);	
+		spanElem.dataset.id = id;
+		let tdElem4 = document.createElement("td");  
+		tdElem4.appendChild(spanElem);
+		trElem.appendChild(tdElem4);
+		
+		checkboxElem.type = "checkbox";
+		checkboxElem.checked = done;
+		if(done){
+			trElem.classList.add("strike");	//체크시 글씨에 줄긋기
+		}else{
+			trElem.classList.remove("strike");	//미체크시 글씨에 줄그은거 지움		
+		}
+		
+		function deleteItem(){
+			trElem.remove();	//row 삭제
+			
+			for(let i = 1; i < todoList.length; i++){
+				if(todoList[i].id == this.dataset.id){	//아이디가 delete id와같으면
+					todoList.splice(i,1);	//i번쨰 인덱스에 있는 1개 지움
+				}
+			}
+			save();	//로컬스토리지에 todoList 저장
+		}
+
+		function checkboxClickCallback(){
+			trElem.classList.toggle("strike");
+			//todo Array에서 done 요소를 찾아 체크박스 선택될시 this.checked true로 
+			//data식별 어떻게? 
+			for(let i = 1; i < todoList.length; i++){
+				if(todoList[i].id == this.dataset.id){
+					todoList[i]["done"] = this.checked;	//체크박스 체크시 done속성 true로 변경 
+				}
+			}
+			save();	//로컬스토리지에 todoList 저장
+		}	
+	}	
 	
+	function _uuid() {	//아이디 랜덤으로 만들기 
+	  var d = Date.now();
+	  if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+	    d += performance.now(); //use high-precision timer if available
+	  }
+	  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+	    var r = (d + Math.random() * 16) % 16 | 0;
+	    d = Math.floor(d / 16);
+	      return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+	  });
+	}
+
+
+	function sortEntry() {	 
+		todoList.sort((a,b) => {
+			let aDate = Date.parse(a.date);	//밀리세컨으로 파싱
+			let bDate = Date.parse(b.date);
+			return aDate - bDate;
+		});	//date 오름차순으로 일정 정렬
+		
+	save();	//sort한거 저장 
+	
+	
+	
+	let table = document.getElementById("todoTable");
+	table.innerHTML = `
+		<tr>
+		<td></td>
+		<td>Date</td>
+		<td>Time</td>
+		<td>to-do</td>
+		<td>delete</td>
+	</tr>`;
+	renderRows();
+
+	}		
+	
+}
+
+
+
+			
+/*			
+			if(flag){
+				//this.style.textDecoration = "line-through";
+				this.classList.add("strike");	//<li>에 class strike 생성해서 css적용됨
+				flag = !flag;
+			}else{
+				//this.style.textDecoration = "none";
+				this.classList.remove("strike");
+				flag = !flag;
+			}		
+		*/
+		
+		
+		
+		
 /*		
 		let liElem = document.createElement("li");
 		
@@ -216,33 +372,6 @@ function todoMain(){
 
 		*/
 		
-		
-		function deleteItem(){
-			liElem.remove();
-		}
-
-		function onClick(){
-			if(flag){
-				//this.style.textDecoration = "line-through";
-				this.classList.add("strike");	//<li>에 class strike 생성해서 css적용됨
-				flag = !flag;
-			}else{
-				//this.style.textDecoration = "none";
-				this.classList.remove("strike");
-				flag = !flag;
-
-			}
-			
-		}
-				
-
-		
-
-	}
-	
-}
-
-
 
 
 
